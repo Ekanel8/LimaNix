6f9497570d602495344ba29b80d9feff188b4ec7
Ekanel8 <gabobus45@gmail.com>
2025-12-04 16:22:21 +0300

    v0.0.6 home ref test 2

--- home.nix
+++ home.nix
@@ -1,26 +1,11 @@
 { config, pkgs, ... }:
 
 {
-  # Home Manager needs a bit of information about you and the paths it should
-  # manage.
   home.username = "doc";
   home.homeDirectory = "/home/doc";
+  home.stateVersion = "25.05"; # Please DO NOT CHANGE
 
-  # This value determines the Home Manager release that your configuration is
-  # compatible with. This helps avoid breakage when a new Home Manager release
-  # introduces backwards incompatible changes.
-  #
-  # You should not change this value, even if you update Home Manager. If you do
-  # want to update the value, then make sure to first check the Home Manager
-  # release notes.
-  home.stateVersion = "25.05"; # Please read the comment before changing.
-
-  # The home.packages option allows you to install Nix packages into your
-  # environment.
   home.packages = [
-    # # Adds the 'hello' command to your environment. It prints a friendly
-    # # "Hello, world!" when run.
-    # pkgs.hello
 
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
@@ -28,88 +13,27 @@
     # # fonts?
     # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
 
-    # # You can also create simple shell scripts directly inside your
-    # # configuration. For example, this adds a command 'my-hello' to your
+    # For example, this adds a command 'my-hello' to your
     # # environment:
     # (pkgs.writeShellScriptBin "my-hello" ''
     #   echo "Hello, ${config.home.username}!"
     # '')
   ];
 
-  # Home Manager is pretty good at managing dotfiles. The primary way to manage
-  # plain files is through 'home.file'.
   home.file = {
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
-    # ".screenrc".source = dotfiles/screenrc;
-
-    # # You can also set the file content immediately.
-    # ".gradle/gradle.properties".text = ''
-    #   org.gradle.console=verbose
-    #   org.gradle.daemon.idletimeout=3600000
-    # '';
+    #smtng for dotfiles
   };
-
   # Home Manager can also manage your environment variables through
-  # 'home.sessionVariables'. These will be explicitly sourced when using a
-  # shell provided by Home Manager. If you don't want to manage your shell
-  # through Home Manager then you have to manually source 'hm-session-vars.sh'
-  # located at either
-  #
-  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  /etc/profiles/per-user/doc/etc/profile.d/hm-session-vars.sh
-  #
   home.sessionVariables = {
     # EDITOR = "emacs";
   };
 
-  # Let Home Manager install and manage itself.
-  programs.home-manager.enable = true;
+  programs.home-manager.enable = true; # Do NOT CHANGE
 
-  programs.fish = {
-    enable = true;
-    shellInit = ''
-      set -g fish_greeting ""
-    '';
+  imports = [
+    ./modules/home/fish.nix
+  ];
 
-    functions = {
-      jarvis = ''
-            set action $argv[1]
-            set state $argv[2]
-
-            switch "$action"
-                case "virtnet"
-                    switch "$state"
-                        case "start"
-                            sudo virsh net-start default
-                        case "stop"
-                            sudo virsh net-destroy default
-                        case "status"
-                            sudo virsh net-list --all
-                    end
-
-                case "switch"
-                    switch "$state"
-                        case "system"
-                            sudo nixos-rebuild -v switch --flake "path:/home/doc/.dotfiles/"
-                        case "home"
-                            home-manager switch --flake ~/.dotfiles/
-                    end
-
-                case "*"
-                    echo "Empty input: $action"
-            end
-      '';
-    };
-  };
 
 
 	#programs.fastfetch = {
@@ -151,9 +75,6 @@
 		color14              = "#8fd3ff";
 		color15              = "#f0f3f6";
 	};
- keybindings = {
-    "ctrl+a" = "select_all";
-  };
   };
  programs.zed-editor = {
     enable = true;

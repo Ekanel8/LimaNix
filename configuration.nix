482b9fe3fba5e9c9a3123e68b0b0d364df5f1ddb
Ekanel8 <gabobus45@gmail.com>
2026-04-01 00:48:02 +0300

    no

--- configuration.nix
+++ configuration.nix
@@ -43,7 +43,6 @@
   font-awesome
 	grim                            # screenshots
 	slurp                           # screenshots
-	alacritty
 	throne
   # <<< Clipboard >>>
 	wl-clipboard
--- home.nix
+++ home.nix
@@ -3,6 +3,5 @@
   home.username = "doc";
   home.homeDirectory = "/home/doc";
   home.stateVersion = "26.05";
-
   imports = [ ./modules/home ];
 }
